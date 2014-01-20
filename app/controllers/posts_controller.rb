553fe92d16a55b6f6215687f73951eefa420c1f3
lyfeyaj <lyfeyaj@gmail.com>
2014-01-20 18:13:10 +0800

    finish back posts functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+#encoding: utf-8
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to action: :index, notice: '博客已经创建'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to action: :index, notice: '博客已修改'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to action: :index, notice: '博客已删除'
+    end
+  end
+
+  private
+
+  def post_params
+    params[:post].permit(:title, :content)
+  end
+end
